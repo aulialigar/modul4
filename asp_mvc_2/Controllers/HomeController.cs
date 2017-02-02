2cc28b53632c6eefbdaa0fa8135f1f91e22aaa11
aulialigar <aulialsh69@gmail.com>
2017-02-02 14:02:10 +0700

    modul2

---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using asp_mvc_2.Security;
+using System.Web.Mvc;
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
